2ebb3c538de0ae8d5f729927985c881f9ad0437d
Terence <68271470+tsamba120@users.noreply.github.com>
2021-08-25 01:21:57 -0400

    8/25/21 - Initial API endpoints test successful

--- song_listens/urls.py
+++ song_listens/urls.py
@@ -1,9 +1,11 @@
-from django.url import include, path
+from django.urls import include, path
 from .views import SongPlaysList, UniqueSongsList, UniqueArtistsList, UniqueAlbumsList
 
-url_patterns = [
+urlpatterns = [
     path('song_plays/', SongPlaysList.as_view(), name='song-plays'),
     path('unique_songs/', UniqueSongsList.as_view(), name='unique-songs'),
     path('unique_artists/', UniqueArtistsList.as_view(), name='unique-artists'),
     path('unique_albums/', UniqueAlbumsList.as_view(), name='unique-albums')
 ]
+
+# Enter return value for home extension
